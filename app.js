5c73b014760aaeda3a9405aea082a03327ef30f1
Harshit Srivastava <harshit@quadrafort.com>
2024-07-22 12:18:06 +0530

    test123456

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jwt-simple');
 const uuid = require('uuid');
+const qs = require('qs');
 
 const app = express();
 const port = 3000;
@@ -13,27 +14,74 @@ const shared_key = '9b3k8q1As6kYNKhcm5CdKCEEWY2SKr3Hup0qLMlDRVnOYNp2';
 app.use(bodyParser.json());
 
 app.post('/api/console-payload', (req, res) => {
-  const payload = req.body;
-  console.log(payload);
 
+  // Authenticate the user
+  const posts = [
+    {
+      user_token: '12345',
+      nombre: 'Jose Altuve',
+      email: 'mejiasoscar1990@gmail.com',
+    },
+    {
+      user_token: '67890',
+      nombre: 'Joseito',
+      email: 'jose@example.com',
+    },
+    {
+      user_token: '1990',
+      nombre: 'Oriana',
+      email: 'oriana@example.com',
+    },
+  ]
 
-  let hardCodedPayload = {
-      "email": "harshitcsrivastava@gmail.com",
-      "name": "Harshit Srivastava",
-      "phone": 9453841101,
-      "tags": [
-          "test"
-      ],
-      "role": "user",
-      iat: (new Date().getTime() / 1000),
-      jti: uuid.v4(),
+  const timestamp = Math.round(new Date().getTime() / 1000)
+  const jtinumber = (timestamp / 100) * 53
+  const jti = Math.round(jtinumber)
+
+  const userToken = req.body.user_token; // Get user_token from the request body
+
+  const username = posts.find((user) => user.user_token === userToken);
+
+  if (!username) {
+    return res.status(401).json({ error: 'User not found' });
   }
 
-  const token = jwt.encode(hardCodedPayload, shared_key);
-  console.log("Token:", token);
+  const user = {
+    name: username.nombre,
+    email: username.email,
+    iat: timestamp,
+    jti: jti,
+  }
 
-  res.status(200).send(`${token}`);
+  const accessToken = jwt.encode(user, shared_key)
+  res.json({ jwt: accessToken })
 });
+
+
+//   let data = qs.stringify({
+//     'user_token': '15566587459485' 
+//   });
+
+
+
+//   let hardCodedPayload = {
+//       "email": "harshitcsrivastava@gmail.com",
+//       "name": "Harshit Srivastava",
+//       "phone": 9453841101,
+//       "tags": [
+//           "test"
+//       ],
+//       "role": "user",
+//       iat: (new Date().getTime() / 1000),
+//       jti: uuid.v4(),
+//   }
+
+//   const token = jwt.encode(hardCodedPayload, shared_key);
+//   console.log("Token:", token);
+
+//   res.status(200).send(`${token}`);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
